7ae3ddefc83d925d606860e4160ef186a06496b8
lenovo_YSU <1074967248@qq.com>
2024-09-24 16:30:51 +0800

    optimize train

--- train.py
+++ train.py
@@ -21,12 +21,20 @@ class Leaner():
             # num_workers=self.arg.num_worker,
             num_workers=0,
             drop_last=True,
+            pin_memory=True,
         )
-        self.model = Model(graph=self.arg.model_args['graph'], graph_args=self.arg.model_args['graph_args'])
+        self.model = Model(graph=self.arg.model_args['graph'],
+                           graph_args=self.arg.model_args['graph_args'])
         self.train_writer = SummaryWriter(os.path.join(arg.log_dir, 'train'), 'train')
         self.global_step = 0
         self.device = torch.device('cuda:{}'.format(self.arg.device))
+        self.optimizer = torch.optim.Adam(
+            self.model.parameters(),
+            lr=float(self.arg.base_lr),
+            weight_decay=self.arg.weight_decay)
         self.loss = torch.nn.CrossEntropyLoss()
+        self.now_acc = 0
+        self.max_acc = 0
 
     def print_log(self, str):
         print(str)
@@ -35,30 +43,55 @@ class Leaner():
         with open('{}/log.txt'.format(self.arg.log_dir), 'a') as f:
             print(str, file=f)
 
-    def save_to_checkpoint(self, filename='weights'):
+
+    def state_dict(self):
+        if hasattr(self.model, 'module') and isinstance(self.model.module, torch.nn.Module):
+            model_state = self.model.module.state_dict()
+        else:
+            model_state = self.model.state_dict()
+        return {
+            'global_step': self.global_step,
+            'model': {k: v.cpu() if isinstance(v, torch.Tensor) else v for k, v in model_state.items()},
+            'optimizer': {k: v.cpu() if isinstance(v, torch.Tensor) else v for k, v in
+                          self.optimizer.state_dict().items()},
+            'max_acc': self.max_acc,
+        }
+
+    def save_to_checkpoint(self, state_dict, filename='weights'):
         if not os.path.exists(self.arg.model_saved_dir):
             os.mkdir(self.arg.model_saved_dir)
         link_name = f'{self.arg.model_saved_dir}/{filename}.pt'
         save_name = f'{self.arg.model_saved_dir}/temp_{filename}.pt'
         if os.path.exists(link_name):
-            torch.save(self.model.state_dict(), save_name)
+            torch.save(state_dict, save_name)
             os.replace(save_name, link_name)
         else:
-            torch.save(self.model.state_dict(), link_name)
+            torch.save(state_dict, link_name)
+
+    def load_from_checkpoint(self):
+        if not os.path.exists(self.arg.model_path):
+            raise FileNotFoundError(f'path of checkpoint does not exist: {self.arg.model_path}')
+        checkpoint = torch.load(self.arg.model_path, map_location=torch.device('cpu'))
+        if hasattr(self.model, 'module') and isinstance(self.model.module, torch.nn.Module):
+            self.model.module.load_state_dict(checkpoint['model'])
+        else:
+            self.model.load_state_dict(checkpoint['model'])
+        self.optimizer.load_state_dict(checkpoint['optimizer'])
+        self.global_step = checkpoint['global_step']
+        self.max_acc = checkpoint['max_acc']
+        print(f'loaded checkpoint from {self.arg.model_path}')
 
     def train(self, epoch):
+        if os.path.exists(self.arg.model_path):
+            self.load_from_checkpoint()
         self.model.to(self.device)
         self.model.train()
-        print(self.model)
-        self.optimizer = torch.optim.Adam(
-            self.model.parameters(),
-            lr=self.arg.base_lr,
-            weight_decay=self.arg.weight_decay)
         self.print_log('Training epoch: {}'.format(epoch))
         self.epoch = epoch
-        loss_value = []
         self.train_writer.add_scalar('epoch', epoch, self.global_step)
         for epoch in range(epoch):
+            loss_value = []
+            acc_value = []
             for data, label in tqdm(self.dataloader, desc='Training progress epoch {}'.format(epoch)):
                 self.global_step += 1
                 # data [N, 12, 300, 17, 2]
@@ -77,19 +110,27 @@ class Leaner():
                 # 写入log
                 loss_value.append(loss.data.item())
                 value, predict_label = torch.max(output.data, 1)
-                acc = torch.mean((predict_label == label.data).float())
-                self.train_writer.add_scalar('acc', acc, self.global_step)
+                self.now_acc = torch.mean((predict_label == label.data).float()).item()
+                acc_value.append(self.now_acc)
+                self.train_writer.add_scalar('acc', self.now_acc, self.global_step)
                 self.train_writer.add_scalar('loss', loss.data.item(), self.global_step)
 
                 # statistics
                 self.lr = self.optimizer.param_groups[0]['lr']
                 self.train_writer.add_scalar('lr', self.lr, self.global_step)
                 if self.global_step % 100 == 0:
-                    self.save_to_checkpoint()
+                    self.save_to_checkpoint(self.state_dict())
+                if self.now_acc > self.max_acc:
+                    self.max_acc = self.now_acc
+                    self.save_to_checkpoint(self.state_dict(), f'weights_acc_{self.max_acc:.4f}')
 
-            self.print_log(
-                '\tMean training loss: {:.4f}.'.format(np.mean(loss_value)))
-            torch.save(self.model.state_dict(), self.arg.model_path)
+            self.print_log(f'\tMean training loss: {np.mean(loss_value):.4f}')
+            self.print_log(f'\tMean training acc: {np.mean(acc_value):.4f}')
+            self.print_log(f'\tmin training acc: {self.max_acc:.4f}')
+
+        self.save_to_checkpoint(self.state_dict(), f'last_weights_{self.now_acc:.4f}')
+        self.print_log(f'\t========== global steps ==========: {self.global_step}')
+
 
 
 if __name__ == '__main__':
